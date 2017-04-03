d2dfcc3c2ca58c37bd252f4074138928a8af4e07
Luca-Ferocino-JCMaxwell-4Bi <ferocinoluca@gmail.com>
2017-04-03 11:48:41 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		System.out.println("*** REGISTRO ELETTRONICO ***\n");
+		System.out.println("*** REGISTRO ELETTRONICO ohm ***\n");
 		
 		Registro accesso = new Registro();
 		
